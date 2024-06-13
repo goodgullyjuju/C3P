5021b0a4f7df3a75c765fc1862d6de5b562b46b7
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-13 17:09:36 -0400

    Create 'Exercise.js'

---
+++ Exercise.js
@@ -0,0 +1,24 @@
+// JavaScript
+const Exercise = {
+    id: '',
+    name: '',
+    description: '',
+    category: '',
+    equipment: '',
+    musclesWorked: [],
+    imageUrl: '',
+    videoUrl: ''
+  };
+  
+  // TypeScript
+  interface Exercise {
+    id: string;
+    name: string;
+    description: string;
+    category: string;
+    equipment: string;
+    musclesWorked: string[];
+    imageUrl: string;
+    videoUrl: string;
+  }
+  
