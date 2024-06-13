c98fd7ee3618567e41e94609ff651bcea6db8856
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-13 19:53:02 -0400

    Create 'WorkoutList.js'

---
+++ WorkoutList.js
@@ -0,0 +1,20 @@
+const WorkoutList = ({ workouts }) => {
+    return (
+      <div>
+        {workouts.map((workout) => (
+          <div key={workout.id}>
+            <h3>{workout.name}</h3>
+            <p>{workout.description}</p>
+            <ul>
+              {workout.exercises.map((exercise) => (
+                <li key={exercise.exerciseId}>
+                  {exercise.exerciseId} - Sets: {exercise.sets}, Reps: {exercise.reps}, Weight: {exercise.weight}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
