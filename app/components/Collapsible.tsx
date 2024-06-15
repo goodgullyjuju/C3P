2010c6bdc0d63fd95029a9514ac0a2fe8cdd3fc9
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-14 20:46:08 -0400

    update 'Collapsible.tsx'

--- app/components/Collapsible.tsx
+++ app/components/Collapsible.tsx
@@ -1,9 +1,10 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
+import { ThemedText } from '../components/navigation/ThemedText';
+import { ThemedView } from '../components/navigation/ThemedView';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+
 import { Colors } from '@/constants/Colors';
 
 export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
