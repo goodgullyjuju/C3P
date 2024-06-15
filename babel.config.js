e93acf889cbd2c9c040d5a821b5be7b1b0d0dcc9
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-15 15:40:05 -0400

    update 'babel.config.js' and 'package.json'

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
+      'babel-preset-expo',{ jsxRuntime: 'automatic' },
       '@babel/preset-env',
       '@babel/preset-react',
       '@babel/preset-typescript',
