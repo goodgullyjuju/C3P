cdbcc323f59b73b666a11d41689689d4231d1d97
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-15 13:29:35 -0400

    update 'package.json'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,8 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/app/hooks/useColorScheme';
+import { useColorScheme } from 'react-native';
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
