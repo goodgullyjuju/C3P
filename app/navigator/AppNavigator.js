3f60628540e4b1189d9e5ad0e7c6eb162fb22efc
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-15 18:28:08 -0400

    update 'AppNavigator.js'

--- app/navigator/AppNavigator.js
+++ app/navigator/AppNavigator.js
@@ -4,11 +4,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import WorkoutDetailsScreen from '../screens/tabs/WorkoutDetailsScreen';
+import WorkoutListScreen from '../screens/tabs/WorkoutListScreen';
 import SignInScreen from '../screens/SignInScreen';
 import ClientDashboard from '../screens/ClientDashboard';
 import CoachDashboard from '../screens/CoachDashboard';
 import ExerciseListScreen from '../screens/ExerciseListScreen';
-import WorkoutListScreen from '../screens/tabs/WorkoutListScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,11 +18,11 @@ export default function AppNavigator() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="WorkoutDetails" component={WorkoutDetailsScreen} />
+        <Stack.Screen name="WorkoutList" component={WorkoutListScreen} />
         <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="ClientDashboard" component={ClientDashboard} />
         <Stack.Screen name="CoachDashboard" component={CoachDashboard} />
         <Stack.Screen name="ExerciseList" component={ExerciseListScreen} />
-        <Stack.Screen name="WorkoutList" component={WorkoutListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
