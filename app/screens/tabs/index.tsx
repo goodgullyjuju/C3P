e61d66edcdd86e96a3fe8ea518ac192852181dec
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-15 17:54:10 -0400

    update 'index.tsx'

--- app/screens/tabs/index.tsx
+++ app/screens/tabs/index.tsx
@@ -6,14 +6,13 @@ import ParallaxScrollView from '@components/ParallaxScrollView';
 import { ThemedText } from '@navigation/ThemedText';
 import { ThemedView } from '@navigation/ThemedView';
 
-
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('../../assets/images/partial-react-logo.png')}
+          source={require('../../../assets/images/partial-react-logo.png')}
           style={styles.reactLogo}
         />
       }>
@@ -24,7 +23,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Edit <ThemedText type="defaultSemiBold">app/screens/tabs/index.tsx</ThemedText> to see changes.
           Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
