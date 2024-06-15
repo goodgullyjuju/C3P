fd7c26d2f574955247c51cfb20c01a5fb6b636e4
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-15 17:25:44 -0400

    update 'index.tsx'

--- app/screens/tabs/index.tsx
+++ app/screens/tabs/index.tsx
@@ -6,13 +6,14 @@ import ParallaxScrollView from '@components/ParallaxScrollView';
 import { ThemedText } from '@navigation/ThemedText';
 import { ThemedView } from '@navigation/ThemedView';
 
+
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@assets/images/partial-react-logo.png')}
+          source={require('../../assets/images/partial-react-logo.png')}
           style={styles.reactLogo}
         />
       }>
