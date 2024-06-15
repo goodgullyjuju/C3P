a94d37f32d7684fdc48541e153595d597c0fc095
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-15 18:26:57 -0400

    update '_layout.tsx'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,14 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
-import React from 'react';
 import { useColorScheme } from 'react-native';
 
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'), // Ensure this path is correct
   });
 
   useEffect(() => {
