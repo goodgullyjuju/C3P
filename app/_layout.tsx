aacab65df0a8b5eb09e1034a78eee9a0d4fa045a
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-19 13:11:16 -0400

    update '_layout.tsx'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
-import { Stack } from 'expo-router';
-import { useCallback } from 'react';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useCallback } from 'react';
+import { useColorScheme } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   const [fontsLoaded] = useFonts({
     'SpaceMonoRegular': require('@/assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -16,9 +19,22 @@ export default function RootLayout() {
     }
   }, [fontsLoaded]);
 
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
     return null;
   }
 
-  return <Stack onLayout={onLayoutRootView} />;
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack onLayout={onLayoutRootView}>
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+        <Stack.Screen name="details" />
+      </Stack>
+    </ThemeProvider>
+  );
 }
