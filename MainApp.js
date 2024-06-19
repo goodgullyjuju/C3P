fdecbf1fc94e9bf5eb98264be6895fa08bbe2a5b
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-19 11:45:28 -0400

    update 'MainApp.js'

--- MainApp.js
+++ MainApp.js
@@ -2,14 +2,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() { // Renamed from MainApp to App
+export default function App() {
   return (
     <NavigationContainer>
-      {/* 
-        Do not include a <Navigator> component here.
-        Expo Router will handle the navigation based on your app's directory structure.
-      */}
+      {/* Navigation is handled based on your app's directory structure */}
     </NavigationContainer>
   );
 }
+
 
