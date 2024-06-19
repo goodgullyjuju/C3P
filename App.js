4f76e5f734ea4d5fc1dd214ccafa703f804db185
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-18 20:39:40 -0400

    update 'App.js'

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-
-
-
-// App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppProvider } from './app/context/AppContext';
 import AppNavigator from './app/navigator/AppNavigator';
 
 export default function App() {
   return (
-    <AppProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AppProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
+
+
