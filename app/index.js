784c2af657e00a675e82ac94f3bc69d03d12876b
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-19 17:05:32 -0400

    update 'index.js'

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ export default function Index() {
             headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
             headerImage={
               <Image
-                source={require('../../../assets/images/partial-react-logo.png')}
+                source={require('../assets/images/partial-react-logo.png')}
                 style={styles.reactLogo}
               />
             }
