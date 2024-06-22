99003bdb494d9cdbd8f9adce34a319342228e93c
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-22 11:57:50 -0400

    update 'metro.config.js'

--- metro.config.js
+++ metro.config.js
@@ -4,11 +4,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 module.exports = (async () => {
   const config = await getDefaultConfig(__dirname);
 
-  
-
   return {
     ...config,
-    
+    transformer: {
+      ...config.transformer,
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    },
     resolver: {
       ...config.resolver,
       assetExts: [...config.resolver.assetExts, 'ttf', 'otf', 'woff', 'woff2'],
