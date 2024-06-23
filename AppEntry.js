c4694ccb0ea22bd3888c50b61811847a4f63f55a
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-23 11:44:39 -0400

    update 'app.json', 'tsconfig.json', 'package-lock.json', 'AppEntry',

--- AppEntry.js
+++ AppEntry.js
@@ -1,5 +1,5 @@
 // AppEntry.js
-
+import 'expo-router/entry';
 import 'expo-dev-client';
 import { registerRootComponent } from 'expo';
 import App from './App'; // Ensure this path is correct and points to your main App component
