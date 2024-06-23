25af5798a516617cb192ab1dd45af60b0dc88c18
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-23 11:17:58 -0400

    rename 'MainNavigation.js', update 'App.js', 'package-lock.json', and 'package.json'

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Text, View } from 'react-native';
-
+import MainNavigation from './MainNavigation'; // Adjust the path if necessary
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
