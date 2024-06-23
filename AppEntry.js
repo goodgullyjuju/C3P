960445ed342010d820b4ccfa2439ac08464e1c4c
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-23 10:25:04 -0400

    update 'AppEntry.js',

--- AppEntry.js
+++ AppEntry.js
@@ -4,3 +4,7 @@ import 'expo-router/entry';
 
 // Import additional necessary modules if needed
 import 'expo-dev-client';
+import { registerRootComponent } from 'expo';
+import App from './app/index'; // Adjust the path if necessary
+
+registerRootComponent(App);
